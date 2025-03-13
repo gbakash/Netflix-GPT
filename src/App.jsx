e0e6df7ee4252ac6d3910157c09640d39665ac98
gbakash <akashgb000@gmail.com>
2025-03-13 10:30:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-700 text-3xl font-bold'>Netflix</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
